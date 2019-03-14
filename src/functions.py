0db4d9b37a30ea6f69a495d5a8f9931e376d773e
oyvind-ege <o.ege@tutanota.de>
2019-03-14 19:07:44 +0100

    Comment to get_me_a_joke()

--- src/functions.py
+++ src/functions.py
@@ -4,6 +4,7 @@ import requests
 header = {"Accept": "text/plain"}
 
 def get_me_a_joke():
+    """Prints a dad joke retrieved from icanhazdadjoke.com API."""
     try:
         r = requests.get('https://icanhazdadjoke.com', headers=header)
         print(r.text)
