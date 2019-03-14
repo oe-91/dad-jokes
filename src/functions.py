48e632b0cb963cd3d3a0a377da84ebe77fd65168
JonathanPS <jonathanpstang@gmail.com>
2019-03-14 19:14:39 +0100

    added main loop to allow user to retrieve more than one joke

--- src/functions.py
+++ src/functions.py
@@ -6,7 +6,7 @@ header = {"Accept": "text/plain"}
 def get_me_a_joke():
     try:
         r = requests.get('https://icanhazdadjoke.com', headers=header)
-        print(r.text)
+        return(r.text)
     except ValueError as err:
         print("Could not connect to icanhazdadjoke.com: ", err)
 
